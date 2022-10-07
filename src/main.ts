1ff58c5a1a93e2348408125f24de9eb790c88ae0
michellopes338 <lopesmichel489@gmail.com>
2022-10-07 17:48:57 -0300

    alterado para express

--- src/main.ts
+++ src/main.ts
@@ -1,18 +1,9 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
-import {
-  FastifyAdapter,
-  NestFastifyApplication,
-} from '@nestjs/platform-fastify';
 import { ValidationPipe } from '@nestjs/common';
 
 async function bootstrap() {
-  const app = await NestFactory.create<NestFastifyApplication>(
-    AppModule,
-    new FastifyAdapter({
-      logger: true,
-    }),
-  );
+  const app = await NestFactory.create(AppModule);
   const whitelist = [
     'http://localhost:3000',
     'http://127.0.0.1:3000',
