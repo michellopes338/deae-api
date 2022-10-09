5081c3342d9c7f0c353c0f41655c0807fc99f380
michellopes338 <lopesmichel489@gmail.com>
2022-10-09 19:51:33 -0300

    ativado cors

--- src/main.ts
+++ src/main.ts
@@ -20,13 +20,18 @@ async function bootstrap() {
     'http://localhost:3000',
     'http://127.0.0.1:3000',
     'http://10.0.0.102:3000',
-    'https://deae-web-next-bkdqf55zh-michellopes338.vercel.app',
+    'https://deae-web-next-bkdqf55zh-michellopes338.vercel.app:443',
+    'https://deae-web-next-bkdqf55zh-michellopes338.vercel.app:8443',
   ];
   const prismaService = app.get(PrismaService);
   await prismaService.enableShutdownHooks(app);
 
   app.enableCors({
-    origin: '*',
+    origin: (origin, callback) => {
+      if (!origin || whitelist.indexOf(origin) !== -1) {
+        callback(null, true);
+      }
+    },
     exposedHeaders: 'Authorization',
   });
   app.useGlobalPipes(
